4e386578e54f2211bd494e12614d785b1ab67c16
Julien Dubois <ptijuju22@gmail.com>
2019-02-12 20:37:29 +0100

    Correcting bugs on Server class

--- network.py
+++ network.py
@@ -30,13 +30,16 @@ class Server:
             return False
 
     def listen(self, max_con_tmp):
+        self.socket.listen(max_con_tmp)
+
         while self.active:
             if len(self.clients) < self.max_client:
-                self.socket.listen(max_connection)
                 con, add = self.socket.accept()
                 self.clients[add] = con
 
-            print("[lemapi] [INFO] [Server.listen] Client '%s' connected" % add)
+                if self.active:
+                    print("[lemapi] [INFO] [Server.listen] Client '%s' connected" \
+                        % add[0])
 
     def start(self, max_con_tmp=0):
         if self.connected:
@@ -48,28 +51,41 @@ class Server:
 
     def stop(self):
         if self.thread:
+            for client in tuple(self.clients):
+                self.disconnect(client)
+
             self.active = False
+            socket.socket().connect((self.address, self.port))
             self.thread.join()
 
-    def send_data(self, data, client=None):
+    def send(self, data, client=None):
         if self.connected:
-            if clients:
+            if client:
                 if client in self.clients:
-                    self.clients[client].send(data.encode())
+                    self.clients[client].send(data)
             else:
                 for connection in tuple(self.clients.values()):
-                    connection.send(data.encode())
+                    connection.send(data)
         else:
             print("[lemapi] [WARNING] [Server.send_data] Server not initialized" \
                 + " yet!")
 
-    def receive_data(self, data, client, buffer=1024):
+    def receive(self, client, buffer=1024):
         if self.connected:
             if client in self.clients:
                 return self.clients[client].recv(buffer)
         else:
             print("[lemapi] [WARNING] [Server.receive_data] Server not initialized" \
                 + " yet!")
+
+    def send_msg(self, msg, client=None):
+        self.send(msg.encode(), client)
+
+    def receive_msg(self, client, buffer=1024):
+        s = self.receive(client, buffer)
+        if s:
+            return s.decode()
+        return ""
 
     def disconnect(self, client):
         if client in self.clients:
@@ -95,21 +111,33 @@ class Client:
         except Exception:
             print("[lemapi] [WARNING] [Client.__init__] Unable to connect to " \
                 + "server '%s' with port '%s'" % (self.address, self.port))
+            if self.connected:
+                self.socket.close()
+            self.connected = False
             return False
 
-    def send_data(self, data):
+    def send(self, data):
         if self.connected:
-            self.socket.send(data.encode())
+            self.socket.send(data)
         else:
-            print("[lemapi] [WARNING] [Client.send_data] Client not initialized" \
+            print("[lemapi] [WARNING] [Client.send] Client not initialized" \
                 + " yet!")
 
-    def receive_data(self, data, buffer=1024):
+    def receive(self, buffer=1024):
         if self.connected:
             return self.socket.recv(buffer)
         else:
-            print("[lemapi] [WARNING] [Client.receive_data] Client not " \
+            print("[lemapi] [WARNING] [Client.receive] Client not " \
                 + "initialized yet!")
+
+    def send_msg(self, data):
+        self.send(data.encode())
+
+    def receive_msg(self, buffer=1024):
+        s = self.receive(buffer)
+        if s:
+            return s.decode()
+        return ""
 
     def disconnect(self):
         if self.connected:
